0667e909edad6b2b57e2e64e1f1e48749e744c9d
hdecode-tuanchau <tuanchau-student@hdexpertise.com>
2021-07-07 22:07:23 +0700

    update

--- src/aws/AwsService.js
+++ src/aws/AwsService.js
@@ -12,13 +12,18 @@ class AwsService {
     this.s3 = new aws.S3()
   }
 
-  async createLinkUpload({ userId, fileName }) {
+  async createLinkUpload({ userId, fileName, fileType }) {
     const s3Params = {
+      ContentType: fileType,
       Bucket: this.S3_BUCKET,
       Key: userId + '/' + fileName,
-      Expires: 500 
+      Expires: 500
     }
-    const urlGetObject = this.s3.getSignedUrl('getObject', s3Params)
+    const urlGetObject = this.s3.getSignedUrl('getObject', {
+      Bucket: this.S3_BUCKET,
+      Key: userId + '/' + fileName,
+      Expires: 500
+    })
     const urlSaveObject = this.s3.getSignedUrl('putObject', s3Params)
 
     return {
